16e26bba854f8a347e47aaace7e5797e9c834af0
usmonovdev <usmonovazizbek045@gmail.com>
2023-11-02 19:46:47 +0500

    testing million js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const { withNextVideo } = require('next-video\process')
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = withNextVideo(nextConfig)
